8f924bb3752ce4fcdd4dfba59a7738a4451fdd1b
simonbrackenboro <sb@lamity.org>
2011-12-07 19:41:47 +0000

    Update git-svnify.sh

--- git-svnify.sh
+++ git-svnify.sh
@@ -1,12 +1,6 @@
 #/bin/bash
-git submodule init
-git submodule update
-for I in Java Files "C#"
-do
 cd "$I"
 git svn init --stdlayout "https://fpml-toolkit.svn.sourceforge.net/svnroot/fpml-toolkit/$I"
 cp .git/refs/remotes/origin/HEAD .git/refs/remotes/trunk
 rm -rf .git/svn
 git svn fetch
-cd ..
-done
