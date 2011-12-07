0be84e9f56846c580c0ea49a8d60be0d8b2b328d
simonbrackenboro <sb@lamity.org>
2011-12-07 19:42:02 +0000

    Update git-svnify.sh

--- git-svnify.sh
+++ git-svnify.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-cd "$I"
-git svn init --stdlayout "https://fpml-toolkit.svn.sourceforge.net/svnroot/fpml-toolkit/$I"
+cd "$1"
+git svn init --stdlayout "https://fpml-toolkit.svn.sourceforge.net/svnroot/fpml-toolkit/$1"
 cp .git/refs/remotes/origin/HEAD .git/refs/remotes/trunk
 rm -rf .git/svn
 git svn fetch
