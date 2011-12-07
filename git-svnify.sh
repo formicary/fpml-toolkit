8c6f9860074d8fe769f842f537096d7e5b9e9120
Simon Brackenboro <sb@lamity.org>
2011-12-07 14:22:51 +0000

    git-svnify.sh corrections

--- git-svnify.sh
+++ git-svnify.sh
@@ -6,6 +6,6 @@ do
 cd "$I"
 git svn init --stdlayout "https://fpml-toolkit.svn.sourceforge.net/svnroot/fpml-toolkit/$I"
 git update-ref refs/remotes/git-svn refs/remotes/origin/master
-git svn rebase
+git svn fetch
 cd ..
 done
